918bce6503390f97b0a9a575ff5b9cfdd4d202c4
kylianlienard <kylian.lienard@supinfo.com>
2025-05-14 14:05:01 +0200

    Arborescence finale. Code ne fonctionne pas totallement

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char **argv)
 {
-    printf("Hello world!\n");
+    printf("fuck off\n");
     scanf("Skiidi?");
     return 0;
 }
--- src/compile.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-folderPath = "C:/Users/kylia/OneDrive - SUPINFO/Documents/GitHub/1PROJ-JVK"
-subprocess.run(["powershell", "-Command", f"gcc '{folderPath}/src/main.c' -o '{folderPath}/main.exe' -I'{folderPath}/SDL2/include' -L'{folderPath}/SDL2/lib' -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer"])
-print("# ~~~ Compilation ended ~~~ #")
